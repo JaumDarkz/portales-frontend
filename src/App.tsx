5391c43a7a406a9c054bbb1d4601ec77f92cdecf
JaumDarkz <viniciuspkfr@gmail.com>
2022-05-20 20:30:22 -0300

    migration commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+
+import Navbar from './components/Navbar'
+import Bridge from './components/Bridge'
+import History from './components/History'
+import Footer from './components/Footer'
+import ContextProvider from './hooks/useContext'
 
 function App() {
+  const [openComponents, setOpenComponents] = useState(false)
+
+  function handleOpen(): void {    
+    setOpenComponents(true)
+  }
+
+  function handleClose(): void {    
+    setOpenComponents(false)
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ContextProvider>
+      <Navbar handleOpen={handleOpen}/>
+      <div>
+        <Bridge />
+        {(<History openHistory={openComponents} handleClose={handleClose} />)}
+      </div>
+      <Footer />
+    </ContextProvider>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Footer.tsx
@@ -0,0 +1,53 @@
+import home from '../assets/svg/home.svg'
+import twitter from '../assets/svg/tt.svg'
+import discord from '../assets/svg/discord.svg'
+import linkedin from '../assets/svg/linkedin.svg'
+import medium from '../assets/svg/medium.svg'
+
+import metamath from '../assets/logo/meta.svg'
+
+function Footer() {
+  return (
+    <div className='flex flex-col items-center 2xl:mt-[3%]'>
+      <div className='flex flex-row justify-center pt-5 space-x-4 w-full'>
+        <button>
+        <div className='p-4 bg-button-gray rounded-xl'>
+          <img src={home} alt="Home" width={15} />
+        </div>
+        </button>
+
+        <button>
+        <div className='p-4 bg-button-gray rounded-xl'>
+          <img src={twitter} alt="Twitter" width={15} />
+        </div> 
+        </button>
+
+        <button>
+        <div className='p-4 bg-button-gray rounded-xl'>
+          <img src={discord} alt="Discord" width={15} />
+        </div>
+        </button>
+
+        <button>
+        <div className='p-4 bg-button-gray rounded-xl'>
+          <img src={linkedin} alt="LinkedIn" width={15} />
+        </div> 
+        </button>
+
+        <button>
+        <div className='p-3 bg-button-gray rounded-xl'>
+          <img className='mt-[25%]' src={medium} alt="Medium" width={20} />
+        </div>
+        </button>
+      </div>
+      <div className='pt-4 text-gray-200 text-sm'> Developed By</div>
+      <div className='pt-3'><img src={metamath} alt='' width={140} /></div>
+      <div className='flex flex-row justify-between space-x-10'>
+        <button className='text-gray-200 underline underline-offset-1'>Terms of Service</button>
+        <button className='text-gray-200 underline underline-offset-1'>Contact Support</button>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
